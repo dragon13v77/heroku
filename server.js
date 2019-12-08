004e99f34d1fa628d482b291d8c3557a31289b97
Dalibor Ristic <diamonddragon13v77@yahoo.com>
2019-12-08 21:47:29 +0100

    First Node App deploy

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Welcome to the simple node application!');
+});
+var server = app.listen(4000, function() {
+	console.log('Your node js server is running');
+});
